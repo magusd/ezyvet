a7cd8e288bdda3e01d575c0103273b70da792f00
magusd <vitor.lobs@gmail.com>
2019-08-07 01:27:01 -0300

    products

--- public/index.php
+++ public/index.php
@@ -5,5 +5,14 @@ require '../src/bootstrap/app.php';
 
 $app = new App();
 
+// ######## please do not alter the following code ########
+$products = [
+    ["name" => "Sledgehammer", "price" => 125.75],
+    ["name" => "Axe", "price" => 190.50],
+    ["name" => "Bandsaw", "price" => 562.131],
+    ["name" => "Chisel", "price" => 12.9],
+    ["name" => "Hacksaw", "price" => 18.45],
+];
+// ########################################################
 
 echo 'yay';
